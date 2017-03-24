474a1b132e55dd7925768b0a2ebebeb510e8c69d
Aaronrbelanger <aaronbelanger@icloud.com>
2017-03-24 19:46:07 -0400

    Added Some more JS

--- 03_24_2017.js
+++ 03_24_2017.js
@@ -1,2 +1,22 @@
 //JavaScript 03/24/2017 
 
+//Learing about types
+var myString = 'This is my string';
+var myNumber = 5;
+var myBoolean = true;
+
+
+// Do not edit the code under this line
+console.log("Name: " + myString);
+console.log("Lucky Number: " + myNumber);
+console.log("Good joke? " + myBoolean);
+
+
+//Messing with console.log 
+console.log('Cheese', "bacon");
+
+//Running some math statements
+console.log(3.5 + 26); //Addition
+console.log(2017 - 1969); //Subtraction
+console.log(65 / 240); //Divison 
+console.log(0.2708333333333333 * 100);
