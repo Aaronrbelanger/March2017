4af3929d20f4bdeab8739d243994cd375abf33e9
Aaronrbelanger <aaronbelanger@icloud.com>
2017-03-24 19:33:22 -0400

    Added JS files

---
+++ 03_23_2017.js
@@ -0,0 +1,17 @@
+//javaScript Learning
+
+//Number Example
+43, 44, 45 103
+
+//String Example
+"Hello this is a string"
+
+    //.length gives you the count of characters in a string
+        "Hello Sting".length
+
+
+
+//Boolean Example
+"I'm a coding like a champ".length > 10;
+
+
