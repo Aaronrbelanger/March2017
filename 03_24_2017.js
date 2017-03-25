c01b8e002eaf12153fe154afea74e2f2550d61eb
Aaronrbelanger <aaronbelanger@icloud.com>
2017-03-24 22:46:19 -0400

    Adding CodeAcademy JS Note

--- 03_24_2017.js
+++ 03_24_2017.js
@@ -86,3 +86,34 @@ if (moonPhase === 'full') {
 } else {
   console.log("I swear I am not a werewolf...");
 }
+
+
+var moonPhase = 'solar eclipse';
+
+if (moonPhase === 'full') {
+  console.log("Howwwwlll!!");
+} else if (moonPhase === "mostly full"){
+  console.log("Arms and legs are getting hairier");
+} else if (moonPhase === "mostly new"){
+  console.log("Back on two feet");
+} else {
+  console.log("Invalid moon phase");
+}
+
+
+var moonPhase = 'full';
+var foggyNight = false;
+
+if (moonPhase === 'full' || foggyNight === true) {
+  console.log("Howwwwlll!!");
+} else if (moonPhase === "mostly full"){
+  console.log("Arms and legs are getting hairier");
+} else if (moonPhase === "mostly new"){
+  console.log("Back on two feet");
+} else {
+  console.log("Invalid moon phase");
+}
+
+
+
+
